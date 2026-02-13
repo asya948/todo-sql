fdcaee0dc9b1974ed1e4a26f792d8820960cca4c
Asya <ahambardzumyan2018@mail.ru>
2026-02-13 16:57:16 +0400

    i make web site

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+import express, {json} from 'express'
+import cors from 'cors'
+import session from 'express-session'
+
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+    session({
+        secret: 'mySecretKey123',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false,
+            maxAge: 1000 * 60 * 60 * 24
+        }
+    })
+)
+
+app.use(
+    cors({
+        origin: 'http://localhost:63342',
+        credentials: true
+    })
+)
+app.use(router)
+
+let users = []
+
+
+app.listen(process.env.APP_PORT || 3000, () => {
+    console.log('server started')
+})
